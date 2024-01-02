b2151b123950f2e1db422fb231e9e8b3cc7d397b
AlessandroSalomonMac <>
2024-01-02 17:41:35 +0100

    110

---
+++ screens/FriendScreen.jsx
@@ -0,0 +1,20 @@
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function FriendScreen({navigation}) {
+    return (
+        <View style={styles.container}>
+            <Text>Deteils</Text>
+            <Button title='Gehe zu Zurück' onPress={() => navigation.navigate("Home")} />
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
