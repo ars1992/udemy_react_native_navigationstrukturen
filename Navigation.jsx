efd5bf34860ed1d253434ef66e607bf3e87884da
AlessandroSalomonMac <>
2024-01-02 12:59:21 +0100

    ende 105 icons

--- Navigation.jsx
+++ Navigation.jsx
@@ -1,7 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './screens/HomeScreen'; './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
+import * as Icon  from '@expo/vector-icons';
 
 const Tap = createBottomTabNavigator()
 
@@ -11,10 +12,16 @@ export default function Navigation() {
         <NavigationContainer>
             <Tap.Navigator>
                 <Tap.Screen name='Home' component={HomeScreen} options={{
-                    title: "Freunde"
+                    title: "Freunde",
+                    tabBarIcon: ({size, color}) => {
+                        return <Icon.Ionicons name="home" size={size} color={color} />
+                    }
                 }} />
                 <Tap.Screen name='Settings' component={SettingsScreen} options={{
-                    title: "Einstellunngen"
+                    title: "Einstellunngen",
+                    tabBarIcon: ({size, color}) => {
+                        return <Icon.Ionicons name="settings" size={size} color={color} />
+                    }
                 }} />
             </Tap.Navigator>
         </NavigationContainer>
