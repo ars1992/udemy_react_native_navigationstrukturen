18955272f7ea2e4ac1bf90af10f85a8eaea38839
AlessandroSalomonMac <>
2024-01-03 16:15:03 +0100

    ende 118 keyExtractor

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -3,16 +3,18 @@ import { Button, StyleSheet, Text, View, FlatList } from 'react-native';
 export default function HomeScreen({ navigation }) {
     return (
         <View style={styles.container}>
-            <FlatList data={[
-                { name: "1" },
-                { name: "2" },
-                { name: "3" },
-                { name: "4" },
-            ]}
+            <FlatList
+                data={[
+                    { name: "1" },
+                    { name: "2" },
+                    { name: "3" },
+                    { name: "4" },
+                ]}
                 renderItem={({ item }) => {
                     return <Button title={`Gehe zu ${item.name}`} onPress={() =>
                         navigation.navigate("Friend", { name: item.name })} />
-                }} />
+                }}
+                keyExtractor={(item) => item.name} />
         </View>
     )
 }
